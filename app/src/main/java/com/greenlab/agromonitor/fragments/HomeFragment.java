53540b643eee6f5238a6c2f14add640e36c45893
mazer <artur.mazer@vistajet.com>
2018-01-18 09:20:45 -0200

    minor changes

--- app/src/main/java/com/greenlab/agromonitor/fragments/HomeFragment.java
+++ app/src/main/java/com/greenlab/agromonitor/fragments/HomeFragment.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 import com.greenlab.agromonitor.HomeActivity;
 import com.greenlab.agromonitor.R;
 
@@ -15,6 +15,7 @@ public class HomeFragment extends Fragment {
 
 
     private Activity mActivity;
+    private Button btnNewProject;
 
     public static Fragment newInstance(){
         Fragment homeFragment = new HomeFragment();
@@ -22,13 +23,25 @@ public class HomeFragment extends Fragment {
     }
 
     public HomeFragment() {
+        mActivity = (HomeActivity)getActivity();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.empty_home, container, false);
+        View view =  inflater.inflate(R.layout.empty_home, container, false);
+
+        btnNewProject = view.findViewById(R.id.btn_new_project);
+        btnNewProject.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return view;
 
     }
 
